a113b04562809e1ff9a528dc86e0ce0bbaa548b7
Diego Rojas <devup2332@gmail.com>
2024-12-17 17:40:42 -0500

    master: Added new response header

--- src/controllers/ContactController.ts
+++ src/controllers/ContactController.ts
@@ -48,6 +48,7 @@ export class ContactController {
           message: "Email was sent successfully",
           status: 200,
         })
+        .writeHead(200, { "access-control-allow-origin": "*" })
         .status(200);
     } catch (err) {
       console.error({ err });
@@ -57,6 +58,7 @@ export class ContactController {
           status: 500,
           err,
         })
+        .writeHead(200, { "access-control-allow-origin": "*" })
         .status(500);
     }
   }
