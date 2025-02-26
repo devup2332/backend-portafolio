8e4c841bd09c309dff1945ebcb05070227cbe095
Diego Rojas <devup2332@gmail.com>
2025-02-25 20:52:34 -0500

    master: Edited

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-process.loadEnvFile();
+process.env.NODE_ENV === "development" && process.loadEnvFile();
 const { API_KEY_SEND_MAILER = "", PORT = 3000, TEMPLATE_ID = "" } = process.env;
 export const env = {
   API_KEY_SEND_MAILER,
